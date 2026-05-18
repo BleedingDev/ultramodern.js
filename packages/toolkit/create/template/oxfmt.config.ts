b7f3eb3a2e482d5e78247eb4cba5fa5abe962b71
Petr Glaser <syreanis+1@gmail.com>
2026-05-18 16:13:07 +0200

    Keep generated UltraModern gates green

--- packages/toolkit/create/template/oxfmt.config.ts
+++ packages/toolkit/create/template/oxfmt.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from "oxfmt";
-import ultracite from "ultracite/oxfmt";
+import { defineConfig } from 'oxfmt';
+import ultracite from 'ultracite/oxfmt';
 
 export default defineConfig({
   extends: [ultracite],
-  ignorePatterns: ["dist", "node_modules", ".modern", ".modernjs", "**/routeTree.gen.ts"],
+  ignorePatterns: ['dist', 'node_modules', '.modern', '.modernjs', '**/routeTree.gen.ts'],
+  singleQuote: true,
 });
